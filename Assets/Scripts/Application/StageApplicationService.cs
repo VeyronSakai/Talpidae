d02a3c37f7f7166a905ed8dffb3424f32b7a7324
VeyronSakai <yuukisakai123@gmail.com>
2021-01-28 23:06:17 +0900

    StageRootを作成し、その子にStageを生成する

--- Assets/Scripts/Application/StageApplicationService.cs
+++ Assets/Scripts/Application/StageApplicationService.cs
@@ -10,16 +10,21 @@ namespace Application
     public sealed class StageApplicationService
     {
         private readonly StageNetworkApplicationService _networkApplicationService;
-        private readonly Transform _stageParent;
+        private readonly Transform _inGameMainTransform;
+        private const string StageRoot = "StageRoot";
+        private Transform _stageRootTransform;
 
-        public StageApplicationService(Transform stageParent)
+        public StageApplicationService(Transform inGameMainTransform)
         {
             _networkApplicationService = new StageNetworkApplicationService();
-            _stageParent = stageParent;
+            _inGameMainTransform = inGameMainTransform;
         }
 
         public async UniTask InitializeStageAsync()
         {
+            var stageRoot = EmptyObjectFactory.Create(StageRoot, _inGameMainTransform);
+            _stageRootTransform = stageRoot.transform;
+
             var stage = await _networkApplicationService.GetStageStartAsync();
             InstantiateStage(stage);
         }
@@ -35,30 +40,37 @@ namespace Application
                     switch (cell.CellType)
                     {
                         case CellType.Hard:
-                            PrefabFactory.Create<HardRockView>(StagePrefabPathDef.HardRock, _stageParent, pos);
+                            PrefabFactory.Create<HardRockView>(StagePrefabPathDef.HardRock, _stageRootTransform, pos);
                             break;
                         case CellType.Medium:
-                            PrefabFactory.Create<MediumRockView>(StagePrefabPathDef.MediumRock, _stageParent, pos);
+                            PrefabFactory.Create<MediumRockView>(StagePrefabPathDef.MediumRock, _stageRootTransform,
+                                pos);
                             break;
                         case CellType.Normal:
-                            PrefabFactory.Create<NormalRockView>(StagePrefabPathDef.NormalRock, _stageParent, pos);
+                            PrefabFactory.Create<NormalRockView>(StagePrefabPathDef.NormalRock, _stageRootTransform,
+                                pos);
                             break;
                         case CellType.Treasure:
-                            PrefabFactory.Create<TreasureCellView>(StagePrefabPathDef.TreasureCell, _stageParent, pos);
+                            PrefabFactory.Create<TreasureCellView>(StagePrefabPathDef.TreasureCell, _stageRootTransform,
+                                pos);
                             break;
                         case CellType.Trap:
                             break;
                         case CellType.ArrowUp:
-                            PrefabFactory.Create<TmpArrowCellView>(StagePrefabPathDef.TmpArrowCell, _stageParent, pos);
+                            PrefabFactory.Create<TmpArrowCellView>(StagePrefabPathDef.TmpArrowCell, _stageRootTransform,
+                                pos);
                             break;
                         case CellType.ArrowDown:
-                            PrefabFactory.Create<TmpArrowCellView>(StagePrefabPathDef.TmpArrowCell, _stageParent, pos);
+                            PrefabFactory.Create<TmpArrowCellView>(StagePrefabPathDef.TmpArrowCell, _stageRootTransform,
+                                pos);
                             break;
                         case CellType.ArrowRight:
-                            PrefabFactory.Create<TmpArrowCellView>(StagePrefabPathDef.TmpArrowCell, _stageParent, pos);
+                            PrefabFactory.Create<TmpArrowCellView>(StagePrefabPathDef.TmpArrowCell, _stageRootTransform,
+                                pos);
                             break;
                         case CellType.ArrowLeft:
-                            PrefabFactory.Create<TmpArrowCellView>(StagePrefabPathDef.TmpArrowCell, _stageParent, pos);
+                            PrefabFactory.Create<TmpArrowCellView>(StagePrefabPathDef.TmpArrowCell,
+                                _stageRootTransform, pos);
                             break;
                         default:
                             throw new ArgumentOutOfRangeException();
