b5ee4dec1740e07cc8ad3cc4a4f89d4f7712e291
takuma0802 <mj.soccer.t.s0802@gmail.com>
2021-01-30 13:59:36 +0900

    inputの基礎作成

--- Assets/Scripts/Main/InGameMain.cs
+++ Assets/Scripts/Main/InGameMain.cs
@@ -1,5 +1,7 @@
 using Application;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UniRx;
 
 namespace Main
 {
@@ -7,26 +9,35 @@ namespace Main
     {
         private StageApplicationService _stageApplicationService;
         private SetUpInGameApplicationService _setUpInGameApplicationService;
+        private IInputProvider _inputProvider;
+
+        private Vector2 _moveDir;
 
         protected override void Inject()
         {
             var inGameMainTransform = transform;
             _stageApplicationService = new StageApplicationService(inGameMainTransform);
             _setUpInGameApplicationService = new SetUpInGameApplicationService(inGameMainTransform);
+            _inputProvider = GetComponent<IInputProvider>();
         }
 
         protected override void OnAwake()
         {
             _stageApplicationService.InitializeStageAsync().Forget();
             _setUpInGameApplicationService.SetUp();
+            _inputProvider.OnAwake();
         }
 
         protected override void OnStart()
         {
+            _inputProvider.MoveDirection.Subscribe(vec => {
+                _moveDir = vec;
+            });
         }
 
         protected override void OnUpdate()
         {
+            Debug.Log("x : " + _moveDir.x + "/ y : " + _moveDir.y);
         }
     }
 }
